d0a77a33105c322bc8eba06e6ebeddd9614c7035
Ashutosh Mishra <ashumisra024@gmail.com>
2022-03-28 04:07:12 -0700

    first commit

---
+++ blog/components/Greet.js
@@ -0,0 +1,8 @@
+import React form 'react'
+
+function Greet(){
+    return <h1>hello ashutosh</h1>
+}
+
+export default Greet 
+ 
---
+++ hello/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet form './components/Greet'
+
+function App() {
+  return (
+    <div className="App">
+     <Greet></Greet> 
+    </div>
+  );
+}
+
+export default App;
